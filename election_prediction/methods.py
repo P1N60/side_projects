1cc45a06a90debecacb76fbaabafbe58a0fc8918
Victor <victor.guldregn@gmail.com>
2025-12-20 18:34:00 +0100

    politics LSTM implemented

--- election_prediction/methods.py
+++ election_prediction/methods.py
@@ -2,4 +2,4 @@ class party:
     def __init__(self, data, party):
         self.party = party
         self.party_data = data[data["party_letter"] == party]
-        self.votes = self.party_data.get("votes")[0]
+        self.current_votes = self.party_data.get("votes").to_list()[len(self.party_data)-1]
