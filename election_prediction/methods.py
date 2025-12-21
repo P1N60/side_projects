c1165000000e84008debf622c0d992627e917f7a
P16G0 <victor.guldregn@gmail.com>
2025-12-21 16:07:28 +0100

    implemented blocks

--- election_prediction/methods.py
+++ election_prediction/methods.py
@@ -1,5 +1,30 @@
-class party:
-    def __init__(self, data, party):
+import numpy as np
+import pandas as pd
+
+class Party:
+    def __init__(self, party: str):
         self.party = party
-        self.party_data = data[data["party_letter"] == party]
-        self.current_votes = self.party_data.get("votes").to_list()[len(self.party_data)-1]
+        self.data = pd.read_csv("voxmeter.csv")
+        self.party_data = self.data[self.data["party_letter"] == party]
+        self.current_votes = self.data.get("votes").to_list()[-1]
+
+class Block:
+    def __init__(self, parties: str):
+        self.parties = parties
+        self.data = pd.read_csv("voxmeter.csv")
+        self.current_votes = 0.0
+    
+    def vote_history(self):
+        block_vote_history = []
+        party_vote_histories = []
+        for party in self.parties:
+            party_class = Party(party)
+            party_vote_histories.append(party_class.party_data.get("votes").to_list())
+        for j in range(len(party_class.party_data)):
+            sum = 0
+            for i in range(len(self.parties)):
+                sum += party_vote_histories[i][j]
+            block_vote_history.append(round(sum, 2))
+        block_vote_history = pd.DataFrame([{"votes": poll} for poll in block_vote_history])
+        return block_vote_history
+
